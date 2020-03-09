8cccc9743e0b2c7af3591d92ae54b940010f1f3d
Parul Gupta <parul.gupta@ksolves.com>
2020-03-09 11:24:48 +0530

    added build

---
+++ build/precache-manifest.b886d39dc528ae1b958f5018cacc44c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e7808467ed0849a69cca7ae4bfe194",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "d4b7fbab88f3079ba6da",
+    "url": "/recipe/static/css/main.17d83cd8.chunk.css"
+  },
+  {
+    "revision": "d5ddeb936d9c2a6dce72",
+    "url": "/recipe/static/js/2.8af33562.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/recipe/static/js/2.8af33562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b7fbab88f3079ba6da",
+    "url": "/recipe/static/js/main.9428203a.chunk.js"
+  },
+  {
+    "revision": "31412e7918281b0c51cf",
+    "url": "/recipe/static/js/runtime-main.3450ed8d.js"
+  }
+]);
